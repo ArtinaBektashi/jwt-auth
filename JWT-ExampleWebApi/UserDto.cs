b79b629e8fdcf076d7bd0477e855e29f3b0e4c13
ArtinaBektashi <ab51034@ubt-uni.net>
2023-02-22 21:08:24 +0100

    web api with jwt auth

---
+++ JWT-ExampleWebApi/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT_ExampleWebApi
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;  
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
